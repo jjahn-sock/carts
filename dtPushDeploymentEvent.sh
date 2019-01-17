0a289a8416997939b26e2c9332f66cc30e4e7e99
Rob Jahn <robjahn@gmail.com>
2019-01-17 15:09:46 +0100

    Create dtPushDeploymentEvent.sh

---
+++ dtPushDeploymentEvent.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+postData=$(cat <<EOF
+    {
+        "eventType" : "CUSTOM_DEPLOYMENT",
+        "source" : "AzureDevops" ,
+        "deploymentName" : "$(Release.DefinitionName)",
+        "deploymentVersion" : "$(Release.ReleaseId)"  ,
+        "deploymentProject" : "$(System.TeamProject)" ,
+        "ciBackLink" : "$(System.TeamFoundationCollectionUri)",
+        "attachRules" : {
+               "tagRule" : [
+                   {
+                       "meTypes":"SERVICE" ,
+                   "tags" : [
+                       {
+                           "context" : "KUBERNETES",
+                           "key": "app",
+                        "value" : "carts"    
+                       },
+                       {
+                           "context" : "CONTEXTLESS",
+                           "key": "environment",
+                        "value" : "$(stageName)"    
+                       }
+                       ]
+                   }
+                   ]
+        }
+}
+EOF
+)
+echo "$postData"
+echo  $(Dynatrace_Tenant)"/api/v1/events"
+Dynatrace_APIURL="$(Dynatrace_Tenant)"
+Dynatrace_APIURL="$Dynatrace_APIURL/api/v1/events"
+echo "$Dynatrace_APIURL"
+curl --url "$Dynatrace_APIURL" -H "Content-type: application/json" -H "Authorization: Api-Token "$(Dynatrace_API_TOKEN) -X POST -d  "$postData"
